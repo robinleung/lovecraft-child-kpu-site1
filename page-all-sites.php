a0c70e3cad3c2404444acf697235d0f979be83ec
Troy Welch <twelch@tru.ca>
2019-08-06 12:47:59 -0700

    Adjust structural tags of all-sites page

--- page-all-sites.php
+++ page-all-sites.php
@@ -5,11 +5,20 @@
 	<div class="section-inner">
 
 		<div class="content">
+
+			<div class="post">
+				<div class="post-inner">
+					<div class="post-content">
+
 			<h2>Listing of All Trubox Sites</h2>
 			<?php
 			// Display subsite listing
 			get_template_part( 'template_parts/all-sites-listing' );
 			?>
+
+						<div>
+							<div>
+								<div>
 
 		</div><!-- .content -->
 
