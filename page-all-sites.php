8d4376367409c53cc2f58dfa1f120024e761c9e5
Troy Welch <twelch@tru.ca>
2019-08-06 13:30:56 -0700

    And messing with body tags now

--- page-all-sites.php
+++ page-all-sites.php
@@ -1,5 +1,31 @@
 <?php get_header(); ?>
 
+<script>
+    if(document.body.classList.contains("home")) {
+        document.body.classList.remove("home");
+    }
+
+    if(document.body.classList.contains("page-template-default")) {
+        document.body.classList.remove("page-template-default");
+    }
+
+    if(!document.body.classList.contains("openetc-all-sites")) {
+        document.body.classList.add("openetc-all-sites");
+    }
+
+    if(!document.body.classList.contains("page-template-full-width-page-template")) {
+        document.body.classList.add("page-template-full-width-page-template");
+    }
+
+    if(!document.body.classList.contains("page-template-full-width-page-template-php")) {
+        document.body.classList.add("page-template-full-width-page-template-php");
+    }
+
+    if(!document.body.classList.contains("page-template")) {
+        document.body.classList.add("page-template");
+    }
+</script>
+
 <div class="wrapper section">
 
 	<div class="section-inner">
