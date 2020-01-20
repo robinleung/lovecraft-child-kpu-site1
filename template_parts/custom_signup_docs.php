b5fb707917958e1f12a92606591c2aec38a6557d
robinleung <robin.leung@kpu.ca>
2020-01-20 14:47:54 -0800

    Update custom_signup_docs.php

--- template_parts/custom_signup_docs.php
+++ template_parts/custom_signup_docs.php
@@ -8,8 +8,8 @@
         document.body.classList.remove("page-template-default");
     }
 
-    if(!document.body.classList.contains("openetc-custom-sign-up")) {
-        document.body.classList.add("openetc-custom-sign-up");
+    if(!document.body.classList.contains("kpu-custom-sign-up")) {
+        document.body.classList.add("kpu-custom-sign-up");
     }
 
     if(!document.body.classList.contains("page-template-full-width-page-template")) {
@@ -35,17 +35,17 @@
 					<article id="custom_signup_wrapper">
 					<div id="custom_signup_docs">
 					<h3>How to create an account and/or get a site</h3>
-						<h4>Step 1: create a Opened login account</h4>
-						<p>If you already have an account on any opened site, please <a href="https://opened.ca/wp-login.php?redirect_to=https%3A%2F%2Ftrubox.ca%2Fwp-signup.php%2F&reauth=1">login</a> and then return to this page.</p>
+						<h4>Step 1: Create a KPU WordPress login account</h4>
+						<p>If you already have an account on the KPU WordPress site, please <a href="https://wordpress.kpu.ca/wp-login.php">login</a> and then return to this page.</p>
 						
-						<p>Note: you must use your institutional email address to create an account login.</p>
-						<h4>Step 2: create a Opened site</h4>
+						<p>Note: you must use your institutional email address to create a login account.</p>
+						<h4>Step 2: Create a KPU WordPress site</h4>
 						<p>Select a site name, you do not have to accept a suggested sitename, you can use your own, but follow the naming restrictions carefully.</p>
 						<p>Carefully select the template that you wish your site to be based upon. (If this site is for a course you are taking the template name will likely have been provided to you by your instructor.)</p>
 						<p>Select your privacy options.</p>
 						<ul>
 							<li>Search engines are internet sites such as Google, Bing, and Duck Duck Go that are used by the general public to locate websites.</li>
-							<li>Registered users of this <strong>network</strong> include any registered user on any Trubox website</li>
+							<li>Registered users of this <strong>network</strong> include any registered user on any KPU WordPress website</li>
 							<li>Registered users/Administrators of this <strong>site</strong> include yourself and users that you specifically give accounts/administrator permissions on your site to. This is a good option to select if you only want yourself and, for instance, your instructor to be able to view your site.</li>
 						</ul>
 					</div>
